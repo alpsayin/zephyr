9158e6b03311464cd7b924a09aabff9f4557dbd9
Alp Sayin <alpsayin@gmail.com>
2023-01-18 15:49:08 +0000

    arch: microblaze: Linker: add linker script and define output format
    Internal references: FWRIVERHD-4980,FWRIVERHD-4989
    
    Signed-off-by: Alp Sayin <alpsayin@gmail.com>

--- include/zephyr/linker/linker-tool-gcc.h
+++ include/zephyr/linker/linker-tool-gcc.h
@@ -53,6 +53,8 @@
 	/* Not needed */
 #elif defined(CONFIG_MIPS)
 	OUTPUT_ARCH("mips")
+#elif defined(CONFIG_MICROBLAZE)
+	OUTPUT_FORMAT("elf32-microblaze")
 #elif defined(CONFIG_ARCH_POSIX)
 	/* Not needed */
 #elif defined(CONFIG_SPARC)
