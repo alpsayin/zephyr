a5799c9b88815089e0cf72599eb9ffc2dc576098
Alp Sayin <alpsayin@gmail.com>
2023-01-18 13:40:31 +0000

    arch: microblaze: IRQ Offload
    Internal references: FWRIVERHD-5108
    
    Signed-off-by: Alp Sayin <alp.sayin@amd.com>

---
+++ arch/microblaze/include/microblaze/emulate_isr.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2022 Advanced Micro Devices, Inc. (AMD)
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
+#ifndef ZEPHYR_ARCH_MICROBLAZE_INCLUDE_MICROBLAZE_EMULATE_ISR_H_
+#define ZEPHYR_ARCH_MICROBLAZE_INCLUDE_MICROBLAZE_EMULATE_ISR_H_
+
+#include <zephyr/arch/microblaze/arch.h>
+
+#include <microblaze/mb_interface.h>
+
+extern void microblaze_emulate_isr(void);
+
+#define EMULATE_ISR()	__asm__ volatile("\tbralid r14, microblaze_emulate_isr\n\tnop\n")
+
+#define EMULATE_IRQ(irq)                                                                           \
+	do {                                                                                       \
+		microblaze_disable_interrupts();                                                   \
+		arch_irq_set_emulated_pending(irq);                                                \
+		__asm__ volatile("\tbralid r14, microblaze_emulate_isr\n\tnop\n");                 \
+	} while (0)
+
+#endif /* ZEPHYR_ARCH_MICROBLAZE_INCLUDE_MICROBLAZE_EMULATE_ISR_H_ */
